671a41a328085fb6546695760d8cc878db84776d
MWUrda <79669541+MWUrda@users.noreply.github.com>
2022-04-04 14:47:21 +0200

    maj

--- Chapt_8_Panel_1/Chapt_8_application/Chapt_8_application_Rincomplet.R
+++ Chapt_8_Panel_1/Chapt_8_application/Chapt_8_application_Rincomplet.R
@@ -5,9 +5,17 @@ estim_pooled_mcg <- pggls(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+p
                           data= data, model="pooling",  index=c("stateid"))
 summary(estim_pooled_mcg)
 
+summary(lm(log(vio)~shall, 
+           data= data))
+
 summary(lm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
            data= data))
 
 estim_pooled <- plm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
                           data= data, model="pooling",  index=c("stateid"))
 summary(estim_pooled)
+
+estim_within <- plm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029 + factor(year), 
+                    data= data, model="within",  index=c("stateid"))
+summary(estim_within)
+
