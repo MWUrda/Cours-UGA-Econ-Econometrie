7d5389ec9be73cfe4871d725662bc23c72b0434f
MWUrda <79669541+MWUrda@users.noreply.github.com>
2022-04-04 14:51:16 +0200

    Update Chapt_8_application_Rincomplet.R

--- Chapt_8_Panel_1/Chapt_8_application/Chapt_8_application_Rincomplet.R
+++ Chapt_8_Panel_1/Chapt_8_application/Chapt_8_application_Rincomplet.R
@@ -10,10 +10,18 @@ summary(lm(log(vio)~shall,
 
 summary(lm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
            data= data))
-
+# "pooled" MCO: random sans MCG(identique à lm)
 estim_pooled <- plm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
                           data= data, model="pooling",  index=c("stateid"))
 summary(estim_pooled)
+
+# MCG
+
+estim_mcg <- pggls(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
+                    data= data, model="pooling",  index=c("stateid"))
+summary(estim_mcg)
+
+# Effets fixes(avec effets temporels sous forme de dummies)
 
 estim_within <- plm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029 + factor(year), 
                     data= data, model="within",  index=c("stateid"))
