00674615676bafd461c1c183d619e1431d2c2dbb
MWUrda <79669541+MWUrda@users.noreply.github.com>
2022-04-04 14:12:32 +0200

    Update Chapt_8_application_Rincomplet.R

--- Chapt_8_Panel_1/Chapt_8_application/Chapt_8_application_Rincomplet.R
+++ Chapt_8_Panel_1/Chapt_8_application/Chapt_8_application_Rincomplet.R
@@ -2,5 +2,12 @@ library(plm)
 library(foreign)
 data <- read.dta("/Volumes/GoogleDrive/Mon Drive/Guns/Guns.dta")
 estim_pooled_mcg <- pggls(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
-                          data= data, model="pooling",  index=c("stateid","year"))
+                          data= data, model="pooling",  index=c("stateid"))
 summary(estim_pooled_mcg)
+
+summary(lm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
+           data= data))
+
+estim_pooled <- plm(log(vio)~shall+incarc_rate+density+avginc+pop+pb1064+pw1064+pm1029, 
+                          data= data, model="pooling",  index=c("stateid"))
+summary(estim_pooled)
